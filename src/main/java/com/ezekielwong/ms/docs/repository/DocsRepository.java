5f680207b593cbab17450a1479728cca8c0c2323
Ezekiel Wong <ezekielw.695@gmail.com>
2023-04-24 21:12:13 +0800

    Added new method to get List<Doc> by status

--- src/main/java/com/ezekielwong/ms/docs/repository/DocsRepository.java
+++ src/main/java/com/ezekielwong/ms/docs/repository/DocsRepository.java
@@ -4,8 +4,12 @@ import com.ezekielwong.ms.docs.entity.Docs;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DocsRepository extends JpaRepository<Docs, String> {
 
     Docs findByCaseId(String caseId);
+
+    List<Docs> findByStatusEquals(String status);
 }
