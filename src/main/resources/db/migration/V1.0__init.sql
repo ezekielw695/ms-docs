e91c5b91bfdfe8e29ae4ea52284a46cefdf07440
Ezekiel Wong <ezekielw.695@gmail.com>
2023-04-24 21:13:29 +0800

    Added flyway dependency

---
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE public.tbl_docs (
+    id uuid NOT NULL,
+    created_by varchar(255) NOT NULL,
+    created_date timestamp NOT NULL,
+    updated_by varchar(255) NULL,
+    updated_date timestamp NULL,
+    case_id varchar(255) NOT NULL,
+    template_name varchar(255) NOT NULL,
+    field_data_list json NOT NULL,
+    requestor_info json NOT NULL,
+    doc_ref_id varchar(255) NULL,
+    metadata varchar NULL,
+    status varchar(255) NULL,
+    CONSTRAINT tbl_docs_pk PRIMARY KEY (id),
+    CONSTRAINT tbl_docs_un UNIQUE (case_id),
+    CONSTRAINT tbl_docs_un2 UNIQUE (doc_ref_id)
+);
+
+CREATE TABLE public.shedlock (
+    name varchar(64) NOT NULL,
+    lock_until timestamp(3) NULL,
+    locked_at timestamp(3) NULL,
+    locked_by varchar(255) NOT NULL,
+    CONSTRAINT shedlock_pk PRIMARY KEY (name)
+);
